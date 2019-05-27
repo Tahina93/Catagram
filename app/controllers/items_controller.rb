b2dd657708786cdf2fb96dd2310a7424f64133b7
SedTheCursed <sed.robert@gmail.com>
2019-05-27 15:40:49 +0200

    Index mostly done

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,8 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'items#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
   resources :carts
   resources :orders
+  devise_for :users
 end
