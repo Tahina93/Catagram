4b51450f4102d7a8240f4950bd2eef3ad9db45a7
lioforest <lioforest@free.fr>
2019-05-27 14:40:30 +0200

    validation items

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,9 @@ class Item < ApplicationRecord
   has_many :buyers, source: :user, class_name: "User", through: :orders
   has_many :potential_buyers, source: :user, class_name: "User", through: :carts
 
+
+  validates :title, presence: true, length: { in: 6..40 }, uniqueness: true
+  validates :description, presence: true, length: { in: 6..1000 }
+  validates :price, presence: true, :numericality => { greater_than_or_equal_to: 0 }    
+
 end
