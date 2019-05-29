7f83ec4b4c80413ca3dc2e9061da2ad15d5de58b
SedTheCursed <sed.robert@gmail.com>
2019-05-29 14:11:46 +0200

    Better routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   root 'items#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items
-  resources :carts
-  resources :orders
+  scope(path_names: {new: "nouveau", edit: "modifier"}) do
+    resources :items, path: 'photo'
+    resources :carts, path: 'panier'
+    resources :orders, path: 'commande'
+    devise_for :users, path: "", :controllers => {:registrations => "registrations"}, :path_names => { :sign_in => 'se-connecter', :sign_out => 'se-deconnecter', :sign_up => 's-enregistrer' }
+    resources :users, only: [:show], path: 'client' do
+      resources :profil_pictures, only: [:create], path: 'avatar'
+    end
 
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users, only: [:show] do
-    resources :profil_pictures, only: [:create]
+   
   end
 
 
