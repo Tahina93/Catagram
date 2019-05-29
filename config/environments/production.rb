8687a3f2dbb5593ae94240c1c454701fcefe0d5c
ragenchirm <noel.gagnaire@gmail.com>
2019-05-29 17:29:04 +0200

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: "https://catagram-lyon-stagging.herokuapp.com/"}
   #config mailer
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: root_url }
+
 
 end
