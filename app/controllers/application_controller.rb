7b43f9212ee3a896f18e308d661b7e3235890201
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-05-29 12:54:09 +0200

    ajout paiement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def get_cart_price
+    total_price =0
+    current_user.potential_items.each do |item|
+      total_price += item.price
+    end
+    total_price
+  end
+  
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,39 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+	before_action :authenticate_user!
 
-  def create
-    order = Order.create(user: current_user)
-    current_user.potential_items.each{|item|
-      order.items.append(item)
-      current_user.cart.items.delete(item)
-    }
-  end
+	def index
+
+	end
+
+	def create
+
+  # Amount in cents
+  @amount = get_cart_price * 100
+
+  customer = Stripe::Customer.create({
+  	email: params[:stripeEmail],
+  	source: params[:stripeToken],
+  })
+
+  charge = Stripe::Charge.create({
+  	customer: customer.id,
+  	amount: @amount.to_i,
+  	description: 'Rails Stripe customer',
+  	currency: 'eur',
+  })
+
+	order = Order.create(user: current_user)
+	current_user.potential_items.each{|item|
+		order.items.append(item)
+		current_user.cart.items.delete(item)
+	}
+
+	redirect_to user_path(current_user.id)
+	flash[:success] = "Merci, votre commande a bien été enregistrée"
+
+	rescue Stripe::CardError => e
+	flash[:error] = e.message
+	redirect_to new_charge_path
+end
 
 end
