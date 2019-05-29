54556bb7ba0cdf1c9112c9bc12138181e34386a8
lioforest <lioforest@free.fr>
2019-05-29 14:57:48 +0200

    base category ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_110701) do
+ActiveRecord::Schema.define(version: 2019_05_29_122933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2019_05_28_110701) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "item_cart_links", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
@@ -68,6 +74,8 @@ ActiveRecord::Schema.define(version: 2019_05_28_110701) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -90,4 +98,5 @@ ActiveRecord::Schema.define(version: 2019_05_28_110701) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "categories"
 end
