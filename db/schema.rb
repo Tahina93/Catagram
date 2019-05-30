b80d0d57b49f5ae987d54d359bf400ab9715f065
lioforest <lioforest@free.fr>
2019-05-30 14:36:53 +0200

    confilct resolved

--- db/schema.rb
+++ db/schema.rb
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_095052) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
