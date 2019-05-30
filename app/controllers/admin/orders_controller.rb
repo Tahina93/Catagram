ad86f2e5fdca4488f123ec5a768f9343bcfa8693
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-05-30 19:42:54 +0200

    maj espace admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,6 +4,15 @@ class Admin::OrdersController < ApplicationController
 	def index
 		@admin = current_user
 		@all_orders = Order.all
+
+		if @all_orders.length > 0
+			array_amount = []
+				@all_orders.each do |order|
+					array_amount << order.amount
+				end
+			@average_amount = array_amount.sum / array_amount.length
+		end
+
 	end
 
 	def show
@@ -15,10 +24,10 @@ class Admin::OrdersController < ApplicationController
 		@order = Order.find(params[:id])
 
 		if @order.destroy
-			flash[:success] = "L'utilisateur a bien été supprimé"
+			flash[:success] = "La commande a bien été supprimé"
 			redirect_back(fallback_location: admin_orders_path)
 		else 
-			flash[:danger] = "Suite à un problème, l'utilisateur n'a pas pu être supprimé"
+			flash[:danger] = "Suite à un problème, la commande n'a pas pu être supprimé"
 			redirect_back(fallback_location: admin_orders_path)
 		end
 	end
