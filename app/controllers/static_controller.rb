26bba0573f6faf969ec52c9299633d416035e101
SedTheCursed <sed.robert@gmail.com>
2019-05-30 22:11:26 +0200

    Legal mentions static pages

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def mentions
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resources :profil_pictures, only: [:create], path: 'avatar'
     end
   resources :charges
+  get '/mentions-legales', :to => 'static#mentions'
 
   end
 
